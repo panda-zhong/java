cf8f7c3f7007890f3ba0df52401ca4d1db2557b8
zhongzis <Berenice@LAPTOP-DC9BRCSG>
2018-09-12 13:28:02 +0800

    testUpdate

---
+++ src/test/java/news/newsMaker/AllTests.java
@@ -0,0 +1,11 @@
+package news.newsMaker;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ NewsMakerServiceTest.class, TestBefore.class })
+public class AllTests {
+	
+}
